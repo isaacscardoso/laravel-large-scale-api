9ee819fedeb14a1946e07e083baa96ed547e9445
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:13:11 -0300

    feat(OrderSku): Implements table relationship methods: order, sku.

--- app/Models/OrderSku.php
+++ app/Models/OrderSku.php
@@ -3,9 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderSku extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'order_id',
+        'sku_id' .
+        'quantity',
+    ];
+
+    /**
+     * Returns the order with the related order sku.
+     *
+     * @return BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * Returns the sku with the related order sku.
+     *
+     * @return BelongsTo
+     */
+    public function sku(): BelongsTo
+    {
+        return $this->belongsTo(Sku::class);
+    }
 }
