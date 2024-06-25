85d644f7bdc6788e621366c9645b39e0c8c65405
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:09:34 -0300

    feat(Brand): It implements the relationship methods between the products table.

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'is_featured',
+    ];
+
+    /**
+     * Returns the brands with their related products.
+     *
+     * @return HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
