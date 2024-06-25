64f02410fa4554312c6de5732192b9924d544330
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:43:47 -0300

    feat(RoleFactory): Implements the dummy data factory.

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -3,9 +3,10 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Role;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
+ * @extends Factory<Role>
  */
 class RoleFactory extends Factory
 {
@@ -17,7 +18,7 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
         ];
     }
 }
