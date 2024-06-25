fd4607347006bb6700c5a5646987fa9185c8600e
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:14:11 -0300

    feat(Sku): Implements table relationship methods: product, images, order_sku.

--- app/Models/Sku.php
+++ app/Models/Sku.php
@@ -3,9 +3,52 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 use Illuminate\Database\Eloquent\Model;
 
 class Sku extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'product_id',
+        'name',
+        'price',
+        'quantity',
+    ];
+
+    /**
+     * Returns the product with the related sku.
+     *
+     * @return BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Returns the images with their related skus.
+     *
+     * @return HasMany
+     */
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    /**
+     * Returns the order skus with their related skus.
+     *
+     * @return HasMany
+     */
+    public function order_skus(): HasMany
+    {
+        return $this->hasMany(OrderSku::class);
+    }
 }
