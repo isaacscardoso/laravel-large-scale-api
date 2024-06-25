baa121554423eafa918ae9562d0207c6b5c666fa
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:11:01 -0300

    feat(Category): It implements the relationship methods between the products table.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'is_featured',
+    ];
+
+    /**
+     * Returns the categories with their related products.
+     *
+     * @return HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
