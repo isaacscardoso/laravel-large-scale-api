af757cfa6d5c3b084eee722b38024156270b75f7
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-25 15:11:13 -0300

    feat(Image): It implements the relationship methods between the sku table.

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,9 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'sku_id',
+        'url',
+        'is_cover',
+    ];
+
+    /**
+     * Returns the sku with the related image.
+     *
+     * @return BelongsTo
+     */
+    public function sku(): BelongsTo
+    {
+        return $this->belongsTo(Sku::class);
+    }
 }
