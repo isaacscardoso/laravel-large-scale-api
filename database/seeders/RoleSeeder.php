562393aca86774f339e93b0a510afd266dc10261
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-28 16:28:41 -0300

    feat: Implements roles seeder.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +12,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Role::factory()->create(['name' => 'administrator']);
+        Role::factory()->create(['name' => 'customer']);
     }
 }
