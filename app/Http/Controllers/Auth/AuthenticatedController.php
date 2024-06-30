dacb7c1a8493e04ceb8bcf0817fbb7d4fc1b19d3
isaacscardoso <isaacscardosoblues@gmail.com>
2024-06-29 21:36:23 -0300

    chore: Adds the Breeze package and modifies Sanctum for API-only authentication, removing data and session authentication.

---
+++ app/Http/Controllers/Auth/AuthenticatedController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\{JsonResponse, Request};
+
+class AuthenticatedController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $token = auth()->user()->createToken('access_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Token removed']);
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\{JsonResponse, Request};
+use Illuminate\Support\Facades\Hash;
+use App\Enums\UserRole;
+use App\Models\User;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'role_id' => UserRole::Customer,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+        ]);
+
+        event(new Registered($user));
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+}
