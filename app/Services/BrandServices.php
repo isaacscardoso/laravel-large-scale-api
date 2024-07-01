2bdec32fa244892b01f1cdc4fdbb048fbae250dc
isaacscardoso <isaacscardosoblues@gmail.com>
2024-07-01 00:44:03 -0300

    feat(BrandService): Implements the service layer and class.

---
+++ app/Services/BrandServices.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Brand;
+use Illuminate\Database\Eloquent\{Model, Builder};
+use App\Http\Requests\{BrandStoreRequest, BrandUpdateRequest};
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class BrandServices
+{
+    /**
+     * Returns a paginated list of Brands.
+     *
+     * @return LengthAwarePaginator
+     */
+    public function list(): LengthAwarePaginator
+    {
+        return Brand::query()->paginate(10);
+    }
+
+    /**
+     * Adds a valid brand to the database.
+     *
+     * @param BrandStoreRequest $request
+     * @return Model|Builder
+     */
+    public function store(BrandStoreRequest $request): Model|Builder
+    {
+        return Brand::query()->create($request->validated());
+    }
+
+    /**
+     * Updates the valid brand data in the database.
+     *
+     * @param BrandUpdateRequest $request
+     * @param Brand $brand
+     * @return bool
+     */
+    public function update(BrandUpdateRequest $request, Brand $brand): bool
+    {
+        return $brand->update($request->validated());
+    }
+
+    /**
+     * Removes the data of a brand from the database.
+     *
+     * @param Brand $brand
+     * @return bool
+     */
+    public function destroy(Brand $brand): bool
+    {
+        return $brand->delete();
+    }
+}
