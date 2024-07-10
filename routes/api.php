f2d6b7bda743a841b195f4b04d9afaec75ac0bad
isaacscardoso <isaacscardosoblues@gmail.com>
2024-07-10 01:41:18 -0300

    feat(api routes): Implements the categories route.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BrandController;
+use App\Http\Controllers\{BrandController, CategoryController};
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResource('brands', BrandController::class)->middleware('auth:sanctum');
+Route::apiResource('categories', CategoryController::class)->middleware('auth:sanctum');
 
 require __DIR__ . '/auth.php';
