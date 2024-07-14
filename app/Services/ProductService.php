167bd19cd3e5b07d42f79642f15138c17735e1c4
isaacscardoso <isaacscardosoblues@gmail.com>
2024-07-14 20:34:59 -0300

    feat(ProductService): Implements the update and delete methods. Refactor the 'path' variable in the 'storeSku' method.

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\{DB, Storage};
 use Illuminate\Database\Eloquent\{Model, Builder};
 use App\Http\Requests\{ProductStoreRequest, ProductUpdateRequest};
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use App\Models\Product;
+use App\Models\{Image, Product, Sku};
 
 class ProductService
 {
@@ -34,7 +34,7 @@ class ProductService
 
         $skus->each(function ($item, $key) use ($skusData, &$imagesToInsert) {
             $images = collect($skusData[$key]['images'])->map(function ($image, $index) use ($item) {
-                $path = Storage::put('products', $image['url']);
+                $path = $image['url']->store('products');
                 return [
                     'sku_id' => $item->id,
                     'url' => $path,
@@ -45,6 +45,45 @@ class ProductService
         });
 
         DB::table('images')->insert($imagesToInsert->toArray());
+    }
+
+    /**
+     * @param Product $product
+     * @param ProductUpdateRequest $request
+     * @return void
+     */
+    private function updateSku(Product $product, ProductUpdateRequest $request): void
+    {
+        $skusData = collect($request['sku']);
+        $existingSkuIds = $product->skus()->pluck('id')->toArray();
+
+        $skusData->each(function ($value) use ($product) {
+            $sku = $product->skus()->updateOrCreate(['id' => $value['id'] ?? 0], $value);
+            if (isset($value['images'])) {
+                $this->updateSkuImages($sku, $value['images']);
+            }
+        });
+
+        $skusToDelete = array_diff($existingSkuIds, $skusData->pluck('id')->filter()->toArray());
+        $product->skus()->whereIn('id', $skusToDelete)->delete();
+    }
+
+    /**
+     * @param Model|Sku $sku
+     * @param array $images
+     * @return void
+     */
+    private function updateSkuImages(Model|Sku $sku, array $images): void
+    {
+        $imageData = array_map(function ($image, $index) {
+            return [
+                'url' => $image['url']->store('products', 's3'),
+                'is_featured' => $index == 0,
+            ];
+        }, $images, array_keys($images));
+
+        $sku->images()->delete();
+        $sku->images()->createMany($imageData);
     }
 
     /**
@@ -75,8 +114,15 @@ class ProductService
      */
     public function update(ProductUpdateRequest $request, Product $product): Product
     {
-        $product->update($request->validated());
-        return $product;
+        return DB::transaction(function () use ($request, $product) {
+            $data = $request->except('sku');
+            $data['slug'] = Str::slug($data['name']);
+            $product->update($data);
+
+            $this->updateSku($product, $request);
+
+            return $product->load('skus.images');
+        });
     }
 
     /**
@@ -87,6 +133,18 @@ class ProductService
      */
     public function destroy(Product $product): bool
     {
-        return $product->delete();
+        return DB::transaction(function () use ($product) {
+            $product->load('skus.images');
+
+            $imageUrls = $product->skus->flatMap(function ($sku) {
+                return $sku->images->pluck('url');
+            })->all();
+
+            Storage::delete($imageUrls);
+            Image::query()->whereIn('sku_id', $product->skus->pluck('id'))->delete();
+            Sku::query()->where('product_id', $product['id'])->delete();
+
+            return $product->delete();
+        });
     }
 }
