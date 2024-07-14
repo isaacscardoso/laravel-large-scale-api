99f6ff49e33878c2a889cf366182c542b935f914
isaacscardoso <isaacscardosoblues@gmail.com>
2024-07-14 16:40:23 -0300

    feat(api routes): Implements the products route.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{BrandController, CategoryController};
+use App\Http\Controllers\{BrandController, CategoryController, ProductController};
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,5 +10,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::apiResource('brands', BrandController::class)->middleware('auth:sanctum');
 Route::apiResource('categories', CategoryController::class)->middleware('auth:sanctum');
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
 
 require __DIR__ . '/auth.php';
